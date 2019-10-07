359305dedd470c90a99064f13c0af52ae7aff172
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2019-10-07 08:59:26 +0200

    updates

--- R/load_practical.R
+++ R/load_practical.R
@@ -1,5 +1,5 @@
 load_practical <- function (name = c("Likelihood", "Basic_Survival", "AFT_Models",
                                      "Cox_Models", "Cox_Extensions")) {
     name <- match.arg(name)
-    learnr::run_tutorial(name, package = "EP03")
+    learnr::run_tutorial(name, package = "EP03_survival")
 }
