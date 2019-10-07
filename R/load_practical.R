363796635363a61be42d860a11abda2c468d6e13
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2019-10-07 09:25:42 +0200

    updates

--- R/load_practical.R
+++ R/load_practical.R
@@ -1,5 +1,11 @@
 load_practical <- function (name = c("Likelihood", "Basic_Survival", "AFT_Models",
                                      "Cox_Models", "Cox_Extensions")) {
     name <- match.arg(name)
+    folder_name <- switch(name,
+                          "Likelihood" = "Practical1",
+                          "Basic_Survival" = "Practical2",
+                          "AFT_Models" = "Practical3",
+                          "Cox_Models" = "Practical4",
+                          "Cox_Extensions" = "Practical5")
     learnr::run_tutorial(name, package = "EP03")
 }
